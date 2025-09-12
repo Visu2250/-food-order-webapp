57046135cbf3d16f5d8dbd16032d642c60ef58db
Visu2250 <goeledu9305@gmail.com>
2025-09-12 20:19:24 +0530

    Add render.yaml

--- Frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -1,25 +1,58 @@
-import React, { useContext } from 'react'
+// import React, { useContext } from 'react'
+// import './FoodDisplay.css';
+// import { StoreContext } from '../../Context/StoreContext';
+// import Fooditem from '../Fooditem/Fooditem';
+
+// const FoodDisplay = ({category}) => {
+
+//     const {food_list}=useContext(StoreContext)
+
+//   return (
+//     <div className='food-display' id='food-display'>
+//         <h2>Top dishes near you</h2>
+//         <div className="food-display-list">
+//             {food_list.map((item,index)=>{
+//               if(category==="All" || category===item.category){
+
+//                 return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+//               }
+//               })}
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default FoodDisplay
+import React, { useContext } from 'react';
 import './FoodDisplay.css';
 import { StoreContext } from '../../Context/StoreContext';
 import Fooditem from '../Fooditem/Fooditem';
 
-const FoodDisplay = ({category}) => {
-
-    const {food_list}=useContext(StoreContext)
+const FoodDisplay = ({ category }) => {
+  const { food_list, url } = useContext(StoreContext);
 
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className="food-display-list">
-            {food_list.map((item,index)=>{
-              if(category==="All" || category===item.category){
-
-                return <Fooditem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-              }
-              })}
-        </div>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <Fooditem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={`${url}/images/${item.image}`}   // ✅ yaha fix
+              />
+            );
+          }
+          return null;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
